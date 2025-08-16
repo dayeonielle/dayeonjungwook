401b5527111e75abd46f1b71961a292f7a136155
Jang Dayeon <dayeonielle@gmail.com>
2025-08-17 02:06:19 +0900

    Setup automatic GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/',  // dayeonielle.github.io는 특수 레포, '/' 그대로
+    base: '/dayeonjungwook/',  // dayeonielle.github.io는 특수 레포, '/' 그대로
 });
